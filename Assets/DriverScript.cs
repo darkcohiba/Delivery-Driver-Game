46c8ebe8cde567eeeb5b41a07ecf571bf7a654db
Sam Waters <samuel.waters@gmail.com>
2023-07-19 13:22:58 -0600

    lesson: introducing variables

--- Assets/DriverScript.cs
+++ Assets/DriverScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DriverScript : MonoBehaviour
 {
 
-    float steerSpeed = 0.1f
+    float steerSpeed = 0.1f;
     float moveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
