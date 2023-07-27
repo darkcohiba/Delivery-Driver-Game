f34ecabb11f64b4406b87d3699a3cd132f8ace4e
Sam Waters <samuel.waters@gmail.com>
2023-07-26 21:09:39 -0600

    lesson: camera follow

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+
+
+    [SerializeField] GameObject car;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = car.transform.position + new Vector3(0, 0, -10);
+
+    }
+}
